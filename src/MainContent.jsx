952d60b85f76fb7238babcd994b8c8d7f13b0d43
ZroC00l <giftgabanakgosi@gmail.com>
2022-01-27 15:52:01 +0200

    added state to the main component + sorted the badge issue

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,8 +1,22 @@
 import { Component } from "react";
+import Badge from "react-bootstrap/Badge";
 
 class MainContent extends Component {
+  /* keyword 'state' is an object that renders data dynamically in real world projects*/
+  // eslint-disable-next-line no-undef
+  state = { pageTitle: "Customers", customersCount: 5 };
+
   render() {
-    return <h1>Hello From Main Content</h1>;
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">
+          {this.state.pageTitle}
+          <span className="m-2">
+            <Badge bg="secondary">{this.state.customersCount}</Badge>
+          </span>
+        </h4>
+      </div>
+    );
   }
 }
 
