8e7b87d4bbebdcc087811b09ce800269972a6f1b
ZroC00l <giftgabanakgosi@gmail.com>
2022-02-07 16:31:17 +0200

    added click event

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -4,7 +4,16 @@ import Badge from "react-bootstrap/Badge";
 class MainContent extends Component {
   /* keyword 'state' is an object that renders data dynamically in real world projects*/
   // eslint-disable-next-line no-undef
-  state = { pageTitle: "Customers", customersCount: 5 };
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5,
+    customers: [
+      { id: 1, name: "Jane", phone: "788 - 2221" },
+      { id: 2, name: "James", phone: "789 - 2222" },
+      { id: 3, name: "Penny", phone: "790 - 3221" },
+      { id: 4, name: "Paul", phone: "791 - 3321" },
+    ],
+  };
 
   render() {
     return (
@@ -14,10 +23,28 @@ class MainContent extends Component {
           <span className="m-2">
             <Badge bg="secondary">{this.state.customersCount}</Badge>
           </span>
+          <button className="btn btn-info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
         </h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Name</td>
+              <td>Phone</td>
+            </tr>
+          </thead>
+        </table>
       </div>
     );
   }
+
+  onRefreshClick = () => {
+    this.setState({
+      customersCount: 6,
+    });
+  };
 }
 
 export default MainContent;
